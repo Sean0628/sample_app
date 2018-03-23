26adc0f6bee01eb392711cabc64b51c9f282cf41
Sho Ito <i.sho.0628@gmail.com>
2018-03-23 20:48:53 +0900

    add several tests to check if users controlller work fine

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -38,8 +39,9 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    log_out
-    redirect_to root_url
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirected_to users_url
   end
 
   private
@@ -62,5 +64,9 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless @user == current_user
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
